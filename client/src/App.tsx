cfa9ace0f2f1ce29e018bc9a95a1a68199fd4d05
hhyuw <high2092@gmail.com>
2022-11-29 23:58:48 +0900

    feat: 클라이언트 라우팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hello, World!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<div>로그인 페이지</div>} />
+        <Route path="/" element={<div>메인 페이지</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
