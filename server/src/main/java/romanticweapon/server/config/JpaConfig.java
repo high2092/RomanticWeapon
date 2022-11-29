9c643d2d4fce669c6bddbc75f18f47d133503795
qsc7342 <qsc7342@hanyang.ac.kr>
2022-11-30 00:32:08 +0900

    feat: 테스트용 객체 추가

---
+++ server/src/main/java/romanticweapon/server/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package romanticweapon.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+//@EnableJpaRepositories(basePackages = "romanticweapon.server.repository")
+public class JpaConfig {
+}
