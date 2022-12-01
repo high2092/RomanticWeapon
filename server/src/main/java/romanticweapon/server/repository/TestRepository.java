d3991a4f55edab3b35c27aa4d7eb6f7955bf48b1
qsc7342 <qsc7342@hanyang.ac.kr>
2022-12-01 21:25:27 +0900

    refactor: 테스트 코드 삭제

--- server/src/main/java/romanticweapon/server/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package romanticweapon.server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import romanticweapon.server.domain.entity.TestEntity;
-
-@Repository
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
