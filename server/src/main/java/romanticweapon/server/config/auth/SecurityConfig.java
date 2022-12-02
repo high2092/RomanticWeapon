3d30fcecb4a2194188f2057aa36f32756ede32c4
qsc7342 <qsc7342@hanyang.ac.kr>
2022-12-03 03:16:17 +0900

    chore: 시큐리티 설정 변경

--- server/src/main/java/romanticweapon/server/config/auth/SecurityConfig.java
+++ server/src/main/java/romanticweapon/server/config/auth/SecurityConfig.java
@@ -1,20 +1,15 @@
 package romanticweapon.server.config.auth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.jfr.Enabled;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import romanticweapon.server.handler.OAuth2SuccessHandler;
-import romanticweapon.server.service.OAuth2UserServiceImpl;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -30,19 +25,12 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/user/login", "/user/register", "/auth/**").permitAll()
+                .antMatchers("/login", "/register", "/auth/**", "/check-login").permitAll()
                 .antMatchers("/user/main").hasRole("USER")
+
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-
-        http
-                .oauth2Login();
-//                .userInfoEndpoint()
-//                .userService(new OAuth2UserServiceImpl());
-//        http
-//                .oauth2Login()
-//                .successHandler(oAuth2SuccessHandler);
 
         return http.build();
     }
