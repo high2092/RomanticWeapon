f40a712e689041172901f5a1bcf2a78640aaed37
hhyuw <high2092@gmail.com>
2022-12-03 18:29:55 +0900

    fix: 배경음악 반복재생 코드 추가

--- client/src/components/BGMController.tsx
+++ client/src/components/BGMController.tsx
@@ -4,6 +4,7 @@ import * as S from './BGMController.style';
 
 const DEFAULT_BGM_VOLUME = 0.5;
 bgm.volume = DEFAULT_BGM_VOLUME;
+bgm.loop = true;
 
 export const BGMController = () => {
   const [playBGM, setPlayBGM] = useState(true);
