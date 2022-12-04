50c024ea47921dcbd0d0193cd4160c0bad0a64c6
hhyuw <high2092@gmail.com>
2022-12-05 00:10:41 +0900

    feat: 로그인 API 연결

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,23 +1,52 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './LoginForm.style';
-import { bgm } from '../constants/constants';
+import { bgm, HOST } from '../constants/constants';
+import { httpPost } from '../utils/utils';
+import { useForm, FieldValues } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const TextContent = {
+  id: '아이디',
+  password: '비밀번호',
+  confirmPassword: '비밀번호 확인',
+  username: '닉네임',
+  helpTextIdRequired: '아이디를 입력하세요',
+  helpTextPasswordRequired: '비밀번호를 입력하세요',
+};
+
+const schema = yup.object().shape({
+  id: yup.string().required(TextContent.helpTextIdRequired),
+  password: yup.string().required(TextContent.helpTextPasswordRequired),
+});
 
 const LoginForm = () => {
   const navigate = useNavigate();
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onTouched',
+    resolver: yupResolver(schema),
+  });
 
-  const handleLoginButtonClick = () => {
-    const loginSuccess = true;
-    if (loginSuccess) {
+  const loginSubmit = async (body: FieldValues) => {
+    const response = await httpPost(`${HOST}/login`, body);
+    if (response.status === 200) {
       bgm.play();
       navigate('/');
     }
   };
 
   return (
-    <S.LoginForm>
+    <S.LoginForm onSubmit={handleSubmit(loginSubmit)}>
       <S.Logo>로맨틱 웨폰</S.Logo>
-      <button onClick={handleLoginButtonClick}>Github 로그인</button>
+      <input {...register('id')}/>
+      <input {...register('password')}/>
+      <button>로그인</button>
     </S.LoginForm>
   );
 };
