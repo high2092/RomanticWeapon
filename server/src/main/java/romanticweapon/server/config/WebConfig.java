6c3212aa2f3b8c931f3aaa6dfd82c30f7f23ead8
qsc7342 <qsc7342@hanyang.ac.kr>
2022-12-05 04:27:53 +0900

    feat: preflight OPTIONS security config 수정

--- server/src/main/java/romanticweapon/server/config/WebConfig.java
+++ server/src/main/java/romanticweapon/server/config/WebConfig.java
@@ -7,10 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://3.85.162.4:8000", "http://27.96.135.22:3000")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowCredentials(true);
     }
 }
