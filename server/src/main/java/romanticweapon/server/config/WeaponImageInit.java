ba06cbdfc88bb56f179420375aa1cb0b8581a28b
qsc7342 <qsc7342@hanyang.ac.kr>
2022-12-05 03:15:29 +0900

    feat: 이미지 초기 주입

--- server/src/main/java/romanticweapon/server/config/WeaponImageInit.java
+++ server/src/main/java/romanticweapon/server/config/WeaponImageInit.java
@@ -2,20 +2,19 @@ package romanticweapon.server.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import romanticweapon.server.domain.entity.WeaponImage;
+import romanticweapon.server.domain.entity.weapon.WeaponImage;
+import romanticweapon.server.domain.enumm.weapon.WeaponType;
 import romanticweapon.server.repository.WeaponImageRepository;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.net.URI;
-import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -45,19 +44,24 @@ public class WeaponImageInit {
             imageFilePathList.add(fullFilePath);
         }
 
+        Collections.sort(imageFileNameList);
+        Collections.sort(imageFilePathList);
+
         for(int i = 0 ; i < imageFileNameList.size() ; i++) {
             String currentFileName = imageFileNameList.get(i);
             String currentFilePath = imageFilePathList.get(i);
 
-            saveImageIfNotExist(currentFileName, currentFilePath);
+            saveImageIfNotExist(currentFileName, currentFilePath, i);
         }
     }
 
-    private void saveImageIfNotExist(String currentFileName, String currentFilePath) {
+    private void saveImageIfNotExist(String currentFileName, String currentFilePath, int idx) {
         if(!weaponImageRepository.findByFileName(currentFileName).isPresent()) {
             WeaponImage weaponImage = WeaponImage.builder()
                     .fileName(currentFileName)
                     .filePath(currentFilePath)
+                    .type(WeaponType.SWORD)
+                    .upgrade((long) idx)
                     .build();
             weaponImageRepository.save(weaponImage);
         }
