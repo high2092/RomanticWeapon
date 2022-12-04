9272fc278b9ee4e01e1f96c6d947b35e49c7e872
qsc7342 <qsc7342@hanyang.ac.kr>
2022-12-05 03:15:39 +0900

    feat: 강화 API 컨트롤러 구현

---
+++ server/src/main/java/romanticweapon/server/controller/enforce/EnforceController.java
@@ -0,0 +1,50 @@
+package romanticweapon.server.controller.enforce;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import romanticweapon.server.domain.dto.response.RefineResponseDto;
+import romanticweapon.server.domain.entity.User;
+import romanticweapon.server.domain.entity.weapon.Weapon;
+import romanticweapon.server.service.auth.UserService;
+import romanticweapon.server.service.enforce.EnforceService;
+import romanticweapon.server.util.SecurityUtil;
+
+import java.sql.Ref;
+
+@RestController
+@RequiredArgsConstructor
+public class EnforceController {
+
+    private final EnforceService enforceService;
+    private final UserService userService;
+
+    @PostMapping("/refine")
+    public ResponseEntity<?> enforce() throws Exception {
+        // 누가 요청했는지 알아야함
+        User userByAuthentication = userService.findUserByAuthentication();
+
+        Weapon beforeWeapon = enforceService.getWeaponByUser(userByAuthentication);
+
+        Weapon enforce = enforceService.enforce(userByAuthentication);
+
+        Boolean isSuccess = false;
+        if(beforeWeapon.getUpgrade() > enforce.getUpgrade()) isSuccess = false;
+        else isSuccess = true;
+
+        RefineResponseDto refineResponseDto = new RefineResponseDto(
+                enforce.getType(),
+                enforce.getUpgrade(),
+                enforce.getEnforceCost(),
+                (int) Math.round((1 - enforce.getUpgrade() * 0.05) * 100),
+                enforce.getPrice(),
+                enforce.getName(),
+                enforce.getWeaponImage().getFilePath(),
+                isSuccess
+        );
+        return new ResponseEntity<>(refineResponseDto, HttpStatus.OK);
+    }
+
+}
