fd173c4dba9aa56d2c0282a522dae8c05445634a
hhyuw <high2092@gmail.com>
2022-12-09 00:25:10 +0900

    feat: 상점 BGM 추가

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -3,8 +3,10 @@ import refineSuccessSoundUrl from '../assets/refine-success.mp3';
 import refineFailureSoundUrl from '../assets/refine-failure.mp3';
 import bgmUrl from '../assets/bgm.mp3';
 import achievementSoundUrl from '../assets/achievement.mp3';
+import shopBgmUrl from '../assets/shop.mp3';
 
 const MAIN_BGM_VOLUME = 0.04;
+const SHOP_BGM_VOLUME = 0.1;
 const REFINE_VOLUME = 0.04;
 const REFINE_SUCCESS_VOLUME = 0.1;
 const REFINE_FAILURE_VOLUME = 0.1;
@@ -36,3 +38,10 @@ export const HOST =
   process.env.NODE_ENV === 'production'
     ? process.env.HOST
     : process.env.DEVHOST;
+
+export const shopBgm = () => {
+  const bgm = new Audio(shopBgmUrl);
+  bgm.volume = SHOP_BGM_VOLUME;
+  bgm.loop = true;
+  return bgm;
+};
