a475846c9efee9f8b440c007cf48a1796e9cf4e9
qsc7342 <qsc7342@hanyang.ac.kr>
2022-12-09 18:58:48 +0900

    feat: ExceptionHandler 적용

--- server/src/main/java/romanticweapon/server/exception/handler/RomanticWeaponExceptionHandler.java
+++ server/src/main/java/romanticweapon/server/exception/handler/RomanticWeaponExceptionHandler.java
@@ -4,9 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import romanticweapon.server.domain.enumm.error.ErrorCode;
 import romanticweapon.server.exception.exception.DuplicateUserIdException;
+import romanticweapon.server.exception.exception.DuplicateUserNameException;
+import romanticweapon.server.exception.exception.NotEnoughGoldException;
 
 @RestControllerAdvice
 @Slf4j
@@ -14,8 +18,7 @@ public class RomanticWeaponExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public void unknownException(Exception e) {
-        log.error("UNKNOWN EXCEPTION : {}", e.getClass().getSimpleName());
-        return;
+        log.error("UNKNOWN EXCEPTION : {}\n{}", e.getClass().getSimpleName(), e.getCause());
     }
 
     @ExceptionHandler(BadCredentialsException.class)
@@ -25,6 +28,20 @@ public class RomanticWeaponExceptionHandler {
 
     @ExceptionHandler(DuplicateUserIdException.class)
     public ResponseEntity duplicateUserIdException(DuplicateUserIdException e) {
-        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity(e.getMessage(), HttpStatus.valueOf(ErrorCode.DUPLICATE_USER_ID.getValue()));
+    }
+
+    @ExceptionHandler(DuplicateUserNameException.class)
+    public ResponseEntity duplicateUserNameException(DuplicateUserNameException e) {
+        return new ResponseEntity(e.getMessage(), HttpStatus.valueOf(ErrorCode.DUPLICATE_USER_NAME.getValue()));
+    }
+    @ExceptionHandler(NotEnoughGoldException.class)
+    public ResponseEntity notEnoughGoldException(NotEnoughGoldException e) {
+        return new ResponseEntity(e.getMessage(), HttpStatus.valueOf(ErrorCode.NOT_ENOUGH_GOLD.getValue()));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity methodNotSupportedException() {
+        return new ResponseEntity(HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
