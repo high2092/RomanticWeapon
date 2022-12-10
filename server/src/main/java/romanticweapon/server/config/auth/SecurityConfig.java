fce200772e1b2a7c3126f702a841dbc974eb5659
qsc7342 <qsc7342@hanyang.ac.kr>
2022-12-10 19:41:31 +0900

    feat: swagger security 예외 추가

--- server/src/main/java/romanticweapon/server/config/auth/SecurityConfig.java
+++ server/src/main/java/romanticweapon/server/config/auth/SecurityConfig.java
@@ -2,6 +2,7 @@ package romanticweapon.server.config.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
 import romanticweapon.server.handler.OAuth2SuccessHandler;
 
 @RequiredArgsConstructor
@@ -18,6 +21,13 @@ public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @Bean
+    @Description("더블 슬래시 허용")
+    public HttpFirewall defaultHttpFirewall() {
+        return new DefaultHttpFirewall();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -27,7 +37,8 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/**/*").permitAll()
-                .antMatchers("/login", "/register", "/auth/**", "/check-login", "/image/**").permitAll()
+                .antMatchers("/swagger-resources/**", "/v3/**").permitAll()
+                .antMatchers("/login", "/register", "/auth/**", "/check-login", "/image/**", "/swagger-ui/**").permitAll()
                 .antMatchers("/user/main").hasRole("USER")
 
                 .anyRequest().authenticated()
