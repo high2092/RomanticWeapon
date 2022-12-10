99d4c1c3aeac6d423868873442d85c9c33ed259c
hhyuw <high2092@gmail.com>
2022-12-10 19:51:12 +0900

    feat: 로그아웃 버튼

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -2,18 +2,33 @@ import React, { useEffect } from 'react';
 import Forge from '../components/Forge';
 import * as S from './MainPage.style';
 import { useNavigate } from 'react-router-dom';
-import { mainBgm } from '../constants/constants';
+import { HOST, mainBgm } from '../constants/constants';
 import dunjeonImageUrl from '../assets/dunjeon.gif';
 import rankingImageUrl from '../assets/ranking.gif';
 import shopImageUrl from '../assets/shop.png';
 import settingImgUrl from '../assets/setting.png';
 import inventoryImgUrl from '../assets/inventory.png';
+import { httpGet } from '../utils/utils';
+
+const httpGetLogout = async () => {
+  const response = await httpGet(`${HOST}/logout`);
+  return response;
+};
 
 const MainPage = () => {
   const navigate = useNavigate();
 
   const handleShopButtonClick = () => {
     navigate('/shop');
+  };
+
+  const handleLogoutButtonClick = async () => {
+    try {
+      const response = await httpGetLogout();
+      if (response.status === 200) navigate('/login');
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -26,6 +41,9 @@ const MainPage = () => {
 
   return (
     <S.MainPage>
+      <S.LogoutButton onClick={handleLogoutButtonClick}>
+        로그아웃
+      </S.LogoutButton>
       <Forge />
 
       <S.Menu>
